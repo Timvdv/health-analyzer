1d89e0c80d47a7582e971253b703cadea6198ee8
Sven de Ronde <ewderonde@gmail.com>
2015-11-24 10:32:42 +0100

    Conflict opgelost

--- www/js/controllers/dashboard.js
+++ www/js/controllers/dashboard.js
@@ -1,9 +1,5 @@
 angular.module('app.controllers').controller('dashboardCtrl', ['$scope', '$http', '$location', function($scope, $http, $location)
 {
     console.log('dashboardCtrl');
-<<<<<<< HEAD
-    
-=======
     $scope.location = $location.path();
->>>>>>> 14bed41982d88749c058db6655d12a0dfa35a0ef
 }]);
