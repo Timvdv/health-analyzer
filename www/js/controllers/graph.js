ea2bdd95265363c926145047d5df77af10667167
Dominic Hoogkamer <Dominichoogkamer@gmail.com>
2015-12-09 21:57:01 +0100

    Dashboard/Graph aangepast

--- www/js/controllers/graph.js
+++ www/js/controllers/graph.js
@@ -5,8 +5,9 @@ angular.module('app.controllers').controller('graphCtrl', ['$scope', '$http', '$
 
 
 
+
         var data = {
-            labels: ["January","June", "July"],
+            labels: ["December","Toekomst"],
             datasets: [
                 {
                     label: "My First dataset",
@@ -20,7 +21,7 @@ angular.module('app.controllers').controller('graphCtrl', ['$scope', '$http', '$
                 },
                 {
                     label: "My Second dataset",
-                    fillColor: "rgba(255, 0, 34, 0.50)",
+                    fillColor: "rgba(255, 0, 80, 0.50)",
                     strokeColor: "#FFF",
                     pointColor: "#FFF",
                     pointStrokeColor: "#fff",
@@ -30,6 +31,31 @@ angular.module('app.controllers').controller('graphCtrl', ['$scope', '$http', '$
                 }
             ]
         };
+
+
+
+
+    var dataBars = {
+        labels: ["December", "Toekomst"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
 
 
         var context = document.getElementById('skills').getContext('2d');
@@ -43,6 +69,7 @@ angular.module('app.controllers').controller('graphCtrl', ['$scope', '$http', '$
 
 
         var myLineChart = new Chart(context).Line(data, options);
+        //var myBarChart = new Chart(context).Bar(dataBars, options);
 
 
 
