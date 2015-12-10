90817233328f78a8252e26e69b5f9e3eb7a19a5a
Dominic Hoogkamer <Dominichoogkamer@gmail.com>
2015-12-10 11:35:53 +0100

    no message

--- www/js/routes.js
+++ www/js/routes.js
@@ -39,15 +39,21 @@ angular.module('app.routes', [])
             })
 
             .state('graph', {
-                url: '/graph-hart',
+                url: '/graph-heart',
                 templateUrl: 'templates/graph.html',
                 controller: 'graphCtrl'
             })
 
             .state('graph-water', {
-                url: '/graph-zuurstof',
+                url: '/graph-liquid',
                 templateUrl: 'templates/graph-water.html',
                 controller: 'graphWaterCtrl'
+            })
+
+            .state('graph-healthy', {
+                url: '/graph-healthy',
+                templateUrl: 'templates/graph-healthy.html',
+                controller: 'graphHealthyCtrl'
             })
 
             .state('nieuweGroep', {
