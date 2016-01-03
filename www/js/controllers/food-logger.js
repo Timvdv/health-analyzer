ec8df8ba1ff27c1583862ce6914e8c97f17769b1
Sven de Ronde <ewderonde@gmail.com>
2016-01-03 23:31:06 +0100

    Added delay to search query

--- www/js/controllers/food-logger.js
+++ www/js/controllers/food-logger.js
@@ -2,8 +2,11 @@ angular.module('app.controllers').controller('foodLoggerCtrl', ['$scope', '$http
 {
     $scope.search = "";
     $scope.details = "no search yet";
-
+    var counter = 0;
     var search = document.getElementById('search');
+    var countdown_interval = null;
+    var itemList = [];
+    
 
     $scope.searchChange = function (event)
     {
@@ -14,4 +17,37 @@ angular.module('app.controllers').controller('foodLoggerCtrl', ['$scope', '$http
             $scope.$apply;
         });
     };
+
+    $scope.searchDelay = function (searchEvent)
+    {
+        clearInterval(countdown_interval);
+        counter = 4;
+        console.log(counter);
+        countdown_interval = setInterval(function(){
+            if(counter>0){
+                counter--;
+                console.log(counter);
+            } else{
+                console.log("Searching...");
+                clearInterval(countdown_interval);
+                $scope.searchChange(searchEvent);
+            }
+        }, 200);
+    };
+
+    // WORK IN PROGRESS.
+    // Momenteel sla ik de ID van het voedsel op ipv de naam, beschrijving etc. zodat ik later een API call 
+    // kan doen met een ID. (Voor het tonen van JOUW eigen lijst) maar weet niet of dit mogelijk/makkelijk is.
+    $scope.addItem = function (item)
+    {
+        console.log(item);
+        itemList.push(item);
+        localStorage["items"] = JSON.stringify(itemList);
+        console.log(localStorage["items"]);
+        var element = document.getElementById(item);
+        element.className += "checked";
+        // Uncomment deze zin om de localstorage te legen.
+        //localStorage["items"] = "";
+        //itemList.push(JSON.parse(localStorage["items"]));
+    }
 }]);
