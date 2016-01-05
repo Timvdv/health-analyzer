e51f16df83cfabaa6753a08c473ba21065f7aae9
D. van Straten <damianvanstraten@hotmail.com>
2016-01-05 15:53:21 +0100

    Vragenlijst werkeeeend

--- www/js/controllers/questionController.js
+++ www/js/controllers/questionController.js
@@ -1,4 +1,33 @@
-angular.module('app.controllers').controller('questionController', ['$scope', '$http', function($scope, $http)
+angular.module('app.controllers').controller('questionController', ['$scope', '$http', '$location', function($scope, $http, $location)
 {
+    if(localStorage.getItem('user') != null) {
+        $location.path('#/home');
+    }
+
+    $scope.formData = {};
+
+    $scope.saveUserData = function () {
+        var name = document.getElementById('questions_name').value;
+        var length = document.getElementById('questions_length').value;
+        var weight = document.getElementById('questions_weight').value;
+        var birthdate = document.getElementById('questions_date').value;
+
+        var sport = $scope.formData.questions_sport;
+        console.log(sport);
+
+        var userData = {
+            name: name,
+            length: length,
+            weight: weight,
+            birthdate: birthdate,
+            sport: sport
+        };
+
+        localStorage.setItem('user',  JSON.stringify(userData));
+        var retrievedObject = localStorage.getItem('user');
+        console.log('retrievedObject: ', JSON.parse(retrievedObject));
+        $location.path('#/home');
+    };
+
     console.log('questionController');
 }]);
