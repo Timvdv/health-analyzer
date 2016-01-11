9abe01294ea6b32c18daebe880bdaf9d1357d39c
D. van Straten <damianvanstraten@hotmail.com>
2016-01-11 16:33:20 +0100

    BMI gebruiker + gegevens veranderen gemaakt

--- www/js/controllers/accountController.js
+++ www/js/controllers/accountController.js
@@ -10,7 +10,7 @@ angular.module('app.controllers').controller('accountController', ['$scope', '$h
     var user_weight = user_data.weight;
     var user_length = user_data.length;
     var user_birthdate = user_data.birthdate;
-    var user_sport = user_data.sport;
+    //var user_sport = user_data.sport;
 
     var name = document.getElementById("questions_name");
     name.value = user_name;
@@ -23,4 +23,61 @@ angular.module('app.controllers').controller('accountController', ['$scope', '$h
 
     var birthdate = document.getElementById("questions_date");
     birthdate.value = user_birthdate;
+
+    var test = 15;
+
+    $scope.formData = {};
+
+    $scope.saveUserData = function()  {
+        var name = document.getElementById("questions_name").value;
+        var length = document.getElementById("questions_length").value;
+        var weight = document.getElementById("questions_weight").value;
+        var birthdate = document.getElementById("questions_date").value;
+
+        var sport = $scope.formData.questions_sport;
+        console.log(sport);
+
+        var userData = {
+            name: name,
+            length: length,
+            weight: weight,
+            birthdate: birthdate,
+            sport: sport
+        };
+
+        localStorage.setItem('user',  JSON.stringify(userData));
+        var retrievedObject = localStorage.getItem('user');
+        console.log('retrievedObject: ', JSON.parse(retrievedObject));
+        location.reload();
+    };
+
+    function checkBMI() {
+        var bmi_result;
+        user_length = user_length / 100;
+        var bmi_calculation = user_weight / user_length / user_length;
+        bmi_calculation = bmi_calculation.toFixed();
+
+        if(bmi_calculation < 18.5) {
+            bmi_result = "Je hebt ondergewicht";
+        }
+        if(bmi_calculation >= 18.5 && test < 25) {
+            bmi_result = "Je hebt een gezond gewicht"
+        }
+        if(bmi_calculation >= 25 && test < 30) {
+            bmi_result = "Je hebt overgewicht"
+        }
+        if(bmi_calculation >= 30 && test < 35) {
+            bmi_result = "Je hebt obesitas"
+        }
+        if(bmi_calculation >= 35) {
+            bmi_result = "Je hebt extreme obesitas"
+        }
+
+        $scope.weight = user_weight;
+        $scope.length = user_length;
+        $scope.bmi_result = bmi_result;
+        $scope.bmi = bmi_calculation;
+    }
+
+    checkBMI();
 }]);
