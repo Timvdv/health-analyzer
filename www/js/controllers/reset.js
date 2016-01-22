e9a0d601fddf908cb04891cb7a0cf6cf3e586114
Sven de Ronde <ewderonde@gmail.com>
2016-01-22 21:06:14 +0100

    Ik heb een reset pagina aangemaakt die de localstorage leeg maakt. Dit kan handig zijn als we tijdens een demo een bepaald ding willen demonstreren. Om de localstorage te clearen moet je ------->  "/reset"  <--------    intypen

---
+++ www/js/controllers/reset.js
@@ -0,0 +1,7 @@
+angular.module('app.controllers').controller('resetCtrl', ['$scope', '$http', '$location', function($scope, $http, $location)
+{
+	console.log("LocalStorage has been cleared!");
+	localStorage.clear();
+    $location.path('#/home');   
+	window.location.reload(true);
+}]);
