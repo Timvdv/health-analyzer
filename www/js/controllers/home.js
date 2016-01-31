4dc14829a7034777eb3679370a5679e878792cd9
Sven de Ronde <ewderonde@gmail.com>
2016-01-31 20:23:20 +0100

    Homepage laat nu dynamisch het aantal calorien en de hoeveelheid vet zien van de afgelopen drie dagen. Als je nu klikt op "Show in Profile" ga je naar de myFood pagina.

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -56,6 +56,297 @@ angular.module('app.controllers').controller('homeCtrl', ['$scope', '$http', '$l
     $scope.bmi = bmi_calculation;
     $scope.bmi_advice = bmi_advice;
 
-    console.log('test');
 
+//===========================================================================================================
+// DYNAMIC HOME CONTENT
+//=========================================================================================================== 
+
+    var getDate = new Date();
+    var currentDate = getDate.toJSON().slice(0,10);
+    var currentMonth = getDate.getMonth()+1;
+
+    var dayRegex = /\d+\-\d+\-(\d+)/ig;
+    var regexCal = dayRegex.exec(currentDate);
+    var currentDay = parseInt(regexCal[1]);
+    
+    var items = [];
+    var tempArray = [];
+    var yesterdayArray = [];
+    var dayBeforeYesterdayArray = [];
+
+    var monthFix = currentMonth;
+    var dayFix = currentDay;
+
+    var todayLength = -1;
+
+    $scope.resfreshArray = function()
+    {
+        console.log("refresh");
+        $scope.lastItemToday = (tempArray.length > 0 ? tempArray[0].name : "Nothing Added Yet.");
+        $scope.lastItemYesterday = (yesterdayArray.length > 0 ? yesterdayArray[0].name : "Nothing Added Yet.");
+        $scope.lastItemDayBeforeYesterday = (dayBeforeYesterdayArray.length > 0 ? dayBeforeYesterdayArray[0].name : "Nothing Added Yet.");
+        $scope.$apply;
+        console.log("Refresh loop: " ,$scope.lastItemToday,$scope.lastItemYesterday,$scope.lastItemDayBeforeYesterday );
+    }
+
+    $scope.numberFix = function()
+    {
+        console.log("numberfix");
+        if(String(currentMonth).length == 1)
+        {
+            monthFix = "0"+currentMonth;
+        }
+        else{
+            monthFix = currentMonth;
+        }
+
+        if(String(currentDay).length == 1)
+        {
+            dayFix = "0"+currentDay;
+        }
+        else{
+            dayFix = currentDay;
+        }
+    }
+    $scope.numberFix();
+    var dateStringToday = "2016-" + monthFix + "-" + dayFix;
+    var dateStringYesterday = "2016-" + monthFix + "-" + (dayFix-1);
+    var dateStringDayBeforeYesterday = "2016-" + monthFix + "-" + (dayFix-2);
+    console.log(dateStringYesterday);
+
+    $scope.today = dateStringToday; 
+    $scope.yesterday = dateStringYesterday;
+    $scope.dayBeforeYesterday = dateStringDayBeforeYesterday;
+    
+
+    $scope.checkForMatch = function(date)
+    {
+        tempArray = [];
+        yesterdayArray = [];
+        dayBeforeYesterdayArray = [];
+        $scope.totalCaloriesToday = 0;
+        $scope.totalFatToday = 0;
+        $scope.totalCaloriesYesterday = 0;
+        $scope.totalFatYesterday = 0;
+        $scope.totalCaloriesDayBeforeYesterday = 0;
+        $scope.totalFatDayBeforeYesterday = 0;
+
+        totalCalories = 0;
+        totalFat = 0;
+
+        if(localStorage['items'])
+        {
+            items = localStorage.items && localStorage.items.length ? JSON.parse(localStorage.items) : []; 
+        }
+        if(localStorage['items']){
+            //===========================================================================================================
+            // Array for TODAY
+            //=========================================================================================================== 
+            for (var i = items[getDate.getMonth()].items.length - 1; i >= 0; i--) {
+                if(items[getDate.getMonth()].items[i].date == date || items[getDate.getMonth()].items[i].date == String(dateStringToday))
+                {
+                    console.log("true!");
+                    tempArray.push(items[getDate.getMonth()].items[i]);
+                    totalCalories += items[getDate.getMonth()].items[i].calories;
+                    totalFat += items[getDate.getMonth()].items[i].fat;
+                    $scope.totalCaloriesToday = totalCalories;
+                    $scope.totalFatToday = totalFat;
+                    todayLength++;
+                    console.log(todayLength);
+
+                    // $scope.lastItemToday = items[getDate.getMonth()].items[i].name;
+                }
+                else if(items[getDate.getMonth()].items[i].date != date || items[getDate.getMonth()].items[i].date != String(dateStringToday))
+                {
+                    console.log("False!");        
+                    $scope.$apply;
+                }
+            }
+            //===========================================================================================================
+            // Array for YESTERDAY
+            //=========================================================================================================== 
+            for (var i = items[getDate.getMonth()].items.length - 1; i >= 0; i--) {
+                if(items[getDate.getMonth()].items[i].date == date || items[getDate.getMonth()].items[i].date == String(dateStringYesterday))
+                {
+                    console.log("true!");
+                    yesterdayArray.push(items[getDate.getMonth()].items[i]);
+                    totalCalories += items[getDate.getMonth()].items[i].calories;
+                    totalFat += items[getDate.getMonth()].items[i].fat;
+                    $scope.totalCaloriesYesterday = totalCalories;
+                    $scope.totalFatYesterday = totalFat;
+                }
+                else if(items[getDate.getMonth()].items[i].date != date || items[getDate.getMonth()].items[i].date != String(dateStringYesterday))
+                {
+                    console.log("False!");          
+                    $scope.$apply;
+                }
+            }
+            //===========================================================================================================
+            // Array for DAY BEFORE YESTERDAY
+            //=========================================================================================================== 
+            for (var i = items[getDate.getMonth()].items.length - 1; i >= 0; i--) {
+                if(items[getDate.getMonth()].items[i].date == date || items[getDate.getMonth()].items[i].date == String(dateStringDayBeforeYesterday))
+                {
+                    console.log("true!");
+                    dayBeforeYesterdayArray.push(items[getDate.getMonth()].items[i]);
+                    totalCalories += items[getDate.getMonth()].items[i].calories;
+                    totalFat += items[getDate.getMonth()].items[i].fat;
+                    $scope.totalCaloriesDayBeforeYesterday = totalCalories;
+                    $scope.totalFatDayBeforeYesterday = totalFat;
+                }
+                else if(items[getDate.getMonth()].items[i].date != date || items[getDate.getMonth()].items[i].date != String(dateStringDayBeforeYesterday))
+                {
+                    console.log("False!");          
+                    $scope.$apply;
+                }               
+            }
+        }
+        $scope.lastItemToday = (tempArray.length > 0 ? tempArray[0].name : "Nothing Added Yet.");
+        $scope.lastItemYesterday = (yesterdayArray.length > 0 ? yesterdayArray[0].name : "Nothing Added Yet.");
+        $scope.lastItemDayBeforeYesterday = (dayBeforeYesterdayArray.length > 0 ? dayBeforeYesterdayArray[0].name : "Nothing Added Yet.");
+        console.log($scope.lastItemToday,$scope.lastItemYesterday,$scope.lastItemDayBeforeYesterday );
+        $scope.resfreshArray();
+    }  
+    $scope.checkForMatch();
+    
+    $scope.itemsToday = tempArray;
+    $scope.itemsYesterday = yesterdayArray;
+    $scope.itemsDayBeforeYesterday = dayBeforeYesterdayArray;
+    
+    $scope.multipleDaysItems = [
+    {
+        "lastItem": ($scope.lastItemToday != null ? $scope.lastItemToday : "Nothing Added Yet."),
+        "totalCalories": $scope.totalCaloriesToday,
+        "totalFat": $scope.totalFatToday
+    },
+    {
+        "lastItem": ($scope.lastItemYesterday !=null ? $scope.lastItemYesterday : "Nothing Added Yet."),
+        "totalCalories": $scope.totalCaloriesYesterday,
+        "totalFat": $scope.totalFatYesterday
+    },
+    {
+        "lastItem": ($scope.lastItemDayBeforeYesterday != null ? $scope.lastItemDayBeforeYesterday : "Nothing Added Yet."),
+        "totalCalories": $scope.totalCaloriesDayBeforeYesterday,
+        "totalFat": $scope.totalFatDayBeforeYesterday
+    }];
+
+    console.log("All Days: ", $scope.multipleDaysItems);
+    console.log("Today: ",$scope.itemsToday);
+    console.log("Yesterday: ", $scope.itemsYesterday);
+    console.log("Day Before Yesterday", $scope.itemsDayBeforeYesterday);
+    $scope.resfreshArray();
+
+    $scope.calorieChecker = function(calToday, fatToday, calYesterday, fatYesterday, calDayBeforeYesterday, fatDayBeforeYesterday)
+    {
+        console.log("calorieChecker");
+
+        var fatAmountToday = (calToday / 100 * 30)/9;
+        var fatAmountYesterday = (calYesterday / 100 * 30)/9;
+        var fatAmountDayBeforeYesterday = (calDayBeforeYesterday / 100 * 30)/9;
+
+        var calTodayButton = document.getElementById('caloriesToday');
+        var fatTodayButton = document.getElementById('fatToday');
+
+        var calYesterdayButton = document.getElementById('caloriesYesterday');
+        var fatYesterdayButton = document.getElementById('fatYesterday');
+
+        var calDayBeforeYesterdayButton = document.getElementById('caloriesDayBeforeYesterday');
+        var fatDayBeforeYesterdayButton = document.getElementById('fatDayBeforeYesterday');
+
+        //=====================================================================================================
+        // TODAY
+        //=====================================================================================================
+        if (calToday >= 0 && calToday <= 1249 || calToday >= 4100)
+        {
+            calTodayButton.className = "negative";
+        }
+        if (calToday >= 1250 && calToday <= 2499 || calToday >= 2900 && calToday <= 4099)
+        {
+            calTodayButton.className = "danger";
+        }
+        if (calToday >= 2500 && calToday <= 2899)
+        {
+            calTodayButton.className = "positive";
+        }
+
+        if (fatToday >= (fatAmountToday*0.95) && fatToday <= (fatAmountToday*1.05))
+        {
+            fatTodayButton.className = "positive";
+        }
+        if (fatToday >= (fatAmountToday*0.80) && fatToday <= (fatAmountToday*0.94) || fatToday >= (fatAmountToday*1.06) && fatToday <= (fatAmountToday*1.20))
+        {
+            fatTodayButton.className = "danger";
+        }
+        if (fatToday <= (fatAmountToday*0.65) || fatToday >= (fatAmountToday*1.21))
+        {
+            fatTodayButton.className = "negative";
+        }
+
+        //=====================================================================================================
+        // YESTERDAY
+        //=====================================================================================================
+
+
+        if (calYesterday >= 0 && calYesterday <= 1249 || calYesterday >= 4100)
+        {
+            calYesterdayButton.className = "negative";
+        }
+        if (calYesterday >= 1250 && calYesterday <= 2499 || calYesterday >= 2900 && calYesterday <= 4099)
+        {
+            calYesterdayButton.className = "danger";
+        }
+        if (calYesterday >= 2500 && calYesterday <= 2899)
+        {
+            calYesterdayButton.className = "positive";
+        }
+
+        if (fatYesterday >= (fatAmountYesterday*0.95) && fatYesterday <= (fatAmountYesterday*1.05))
+        {
+            fatYesterdayButton.className = "positive";
+        }
+        if (fatYesterday >= (fatAmountYesterday*0.80) && fatYesterday <= (fatAmountYesterday*0.94) || fatYesterday >= (fatAmountYesterday*1.06) && fatYesterday <= (fatAmountYesterday*1.20))
+        {
+            fatYesterdayButton.className = "danger";
+        }
+        if (fatYesterday <= (fatAmountYesterday*0.65) || fatYesterday >= (fatAmountYesterday*1.21))
+        {
+            fatYesterdayButton.className = "negative";
+        }
+
+        //=====================================================================================================
+        // DAY BEFORE YESTERDAY
+        //=====================================================================================================
+
+
+        if (calDayBeforeYesterday >= 0 && calDayBeforeYesterday <= 1249 || calDayBeforeYesterday >= 4100)
+        {
+            calDayBeforeYesterdayButton.className = "negative";
+        }
+        if (calDayBeforeYesterday >= 1250 && calDayBeforeYesterday <= 2499 || calDayBeforeYesterday >= 2900 && calDayBeforeYesterday <= 4099)
+        {
+            calDayBeforeYesterdayButton.className = "danger";
+        }
+        if (calDayBeforeYesterday >= 2500 && calDayBeforeYesterday <= 2899)
+        {
+            calDayBeforeYesterdayButton.className = "positive";
+        }
+
+        if (fatDayBeforeYesterday >= (fatAmountDayBeforeYesterday*0.95) && fatDayBeforeYesterday <= (fatAmountDayBeforeYesterday*1.05))
+        {
+            fatDayBeforeYesterdayButton.className = "positive";
+        }
+        if (fatDayBeforeYesterday >= (fatAmountDayBeforeYesterday*0.80) && fatDayBeforeYesterday <= (fatAmountDayBeforeYesterday*0.94) || fatDayBeforeYesterday >= (fatAmountDayBeforeYesterday*1.06) && fatDayBeforeYesterday <= (fatAmountDayBeforeYesterday*1.20))
+        {
+            fatDayBeforeYesterdayButton.className = "danger";
+        }
+        if (fatDayBeforeYesterday <= (fatAmountDayBeforeYesterday*0.65) || fatDayBeforeYesterday >= (fatAmountDayBeforeYesterday*1.21))
+        {
+            fatDayBeforeYesterdayButton.className = "negative";
+        }
+        console.log((fatAmountToday*0.95));
+    }
+
+    $scope.calorieChecker($scope.totalCaloriesToday, $scope.totalFatToday, $scope.totalCaloriesYesterday, $scope.totalFatYesterday, $scope.totalCaloriesDayBeforeYesterday, $scope.totalFatDayBeforeYesterday);
+
+   
 }]);
